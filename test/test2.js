a9e5045a966e411b1f0aee751afc3010424d541d
Matvii Hodovaniuk <matvii@hodovani.uk>
2020-10-02 13:45:15 +0300

    Add test

--- test/test2.js
+++ test/test2.js
@@ -1,26 +1,5 @@
-// const usbDetect = require('usb-detection')
-// const usb = require('usb')
+import exsitProjectFile from "../src/utils/path";
 
-// const drivelist = require('drivelist')
-
-// drivelist.list().then((drives) => {
-//   console.log(JSON.stringify(drives))
-// })
-
-// usbDetect.startMonitoring()
-
-// // console.log(usb.getDeviceList())
-// usbDetect.find('8137', '147', (err, device) => {
-//   console.log('add', device)
-
-//   // console.log(usb.)
-//   // console.log(usb)
-// })
-
-function sum(a, b) {
-  return a + b;
-}
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("path fails", () => {
+  expect(exsitProjectFile("")).toBe(false);
 });
