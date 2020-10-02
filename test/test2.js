5abd2d4b3974105b553141b8a0c6e3e5ef913d33
Matvii Hodovaniuk <matvii@hodovani.uk>
2020-10-02 16:30:15 +0300

    Add deleted files

---
+++ test/test2.js
@@ -0,0 +1,18 @@
+const usbDetect = require("usb-detection");
+const usb = require("usb");
+
+const drivelist = require("drivelist");
+
+drivelist.list().then((drives) => {
+  console.log(JSON.stringify(drives));
+});
+
+usbDetect.startMonitoring();
+
+// console.log(usb.getDeviceList())
+usbDetect.find("8137", "147", (err, device) => {
+  console.log("add", device);
+
+  // console.log(usb.)
+  // console.log(usb)
+});
