d9fe12eb191e970be10db327dc3b8978238280e4
2ndream <ider@madmachine.io>
2021-06-13 14:13:27 +0800

    feat: download remote examples

--- config/scripts/postinstall.js
+++ config/scripts/postinstall.js
@@ -1,5 +1,6 @@
 const fse = require('fs-extra')
 const path = require('path')
+const childProcess = require('child_process')
 const { downloadLatest } = require('./sdkDownloader')
 
 const rootDir = path.resolve(__dirname, '../..')
@@ -28,9 +29,23 @@ async function downloadSdk(platform, releaseOnly = false) {
   })
 }
 
+function downloadExamples() {
+  const targetDir = path.resolve(rootDir, 'resources', 'project', 'Examples')
+  if (!fse.existsSync(targetDir)) {
+    fse.mkdirSync(targetDir)
+  }
+  childProcess.execSync(`git clone --depth 1 https://github.com/madmachineio/MadExamples.git ${targetDir}`)
+}
+
 (async () => {
+  console.log('Step1. Downloading sdk')
   await downloadSdk('mac')
   await downloadSdk('win', true)
-  // todo 解不开，有问题
+  // todo unzip error
   // await downloadSdk('linux', true)
+  console.log('Step1 Done.')
+
+  console.log('Step2. Downloading examples')
+  downloadExamples()
+  console.log('Step2 Done.')
 })()
